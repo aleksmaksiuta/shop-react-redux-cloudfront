097f87133494022a513883d68891486307f1691f
aleksmaksiuta <aleks.maksiuta@gmail.com>
2021-07-12 16:46:30 +0300

    chore: changed API to AWS Gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://a950j0y0ld.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://a950j0y0ld.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://a950j0y0ld.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://a950j0y0ld.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://a950j0y0ld.execute-api.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
