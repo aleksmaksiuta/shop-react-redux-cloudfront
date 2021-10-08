c011c0d98a7766d7b33ec6c00a363c37d828d972
aleksmaksiuta <aleks.maksiuta@gmail.com>
2021-10-08 16:51:01 +0300

    feat: [module8] updated cart URL;

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://aleksmaksiuta-rs-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/',
 };
 
 export default API_PATHS;
