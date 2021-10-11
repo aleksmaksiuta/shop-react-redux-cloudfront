77415c9f915006948b51c4994846963c9a245b0a
aleksmaksiuta <aleks.maksiuta@gmail.com>
2021-10-11 16:09:17 +0300

    feat: [module9] updated URL to match bff;

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,8 +3,8 @@ const API_PATHS = {
   product: 'http://aleksmaksiuta-bff-api-develop.eu-west-1.elasticbeanstalk.com/product',
   order: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://a950j0y0ld.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://aleksmaksiuta-bff-api-develop.eu-west-1.elasticbeanstalk.com/cart',
+  bff: 'http://aleksmaksiuta-bff-api-develop.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://aleksmaksiuta-rs-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/',
 };
 
 export default API_PATHS;
